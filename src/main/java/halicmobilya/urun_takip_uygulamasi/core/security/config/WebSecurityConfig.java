ea339f045d90587b4e2265b7e70217b08b11868b
Abdulkadir Eyigül <aeyigul@outlook.com>
2022-07-06 00:02:35 +0300

    web config updated

--- src/main/java/halicmobilya/urun_takip_uygulamasi/core/security/config/WebSecurityConfig.java
+++ src/main/java/halicmobilya/urun_takip_uygulamasi/core/security/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().authorizeRequests().antMatchers(
                 "/api/v1/auth/*",
-                "/api/v1/departments/addDepartment"
+                "/api/v1/departments/addDepartment",
                 "/v2/api-docs",
                 "/swagger-resources",
                 "/swagger-resources/**",
